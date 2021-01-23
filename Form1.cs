b0c439af5382849b66cf87662420f4f567b40fa5
Emily Matson <EKMatson@student.fullsail.edu>
2021-01-22 20:23:11 -0600

    Adding Count NeighborsFinite Method

--- Form1.cs
+++ Form1.cs
@@ -39,8 +39,67 @@ namespace GOL_EM_14
         }
 
         // Calculate the next generation of cells
+        private int CountNeighborsFinite(int x, int y)
+        {
+            int count = 0;
+            int xLength = universe.GetLength(0);
+            int yLength = universe.GetLength(1);
+
+            for (int yOffset = -1; yOffset <= 1; yOffset++)
+            {
+                for (int xOffset = -1; xOffset <= 1; xOffset++)
+                {
+                    int xCheck = x + xOffset;
+                    int yCheck = y + yOffset;
+
+                    // if xOffset and yOffset are both equal to 0 then continue
+                    if (xOffset == 0 & yOffset == 0)
+                    {
+                        continue;
+                    }
+
+
+                    // if xCheck is less than 0 then continue
+                    if (xCheck < 0)
+                    {
+                        continue;
+
+                    }
+
+                    // if yCheck is less than 0 then continue
+                    if (yCheck < 0)
+                    {
+                        continue;
+
+                    }
+
+                    // if xCheck is greater than or equal too xLen then continue
+                    if (xCheck >= xLength)
+                    {
+                        continue;
+                    }
+
+                    // if yCheck is greater than or equal too yLen then continue
+                    if (yCheck >= yLength)
+                    {
+                        continue;
+                    }
+
+                    if (universe[xCheck, yCheck] == true)
+                        count++;
+                   
+                }
+
+            }
+
+            return count;
+        }
+
+
         private void NextGeneration()
         {
+            
+
 
 
             // Increment generation count
