bbc4f8378670b636e59a460d23d0709a1b3e30c7
Emily Matson <EKMatson@student.fullsail.edu>
2021-04-27 16:16:37 -0500

    Adding Random int Seed

--- Random.cs
+++ Random.cs
@@ -11,10 +11,23 @@ namespace GOL_EM_14
 {
     public class Random :Form1
     {
-        private void randomizeUniverseToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private NumericUpDown numericUpDown1;
+        //stack over flow random seed gen 
 
+       public Random(int Seed)
+        {
+            int num = 161803398 - (Seed == int.MinValue ? int.MaxValue : Math.Abs(Seed));
         }
+        public Decimal RandomSeed
+        {
+            get => (Decimal)(int)numericUpDown1.Value;
+            set => numericUpDown1.Value = value;
+        }
+
+        private void randomizeUniverseToolStripMenuItem_Click(object sender, EventArgs e) => RandomSeed = (Decimal)new Random().Next(int MinValue, int MaxValue);
+
+
+
         private void InitializeComponent()
         {
 
